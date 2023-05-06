536d3d035c9557d1e6fbc2f16383314034f2d9fd
Anggietha Santoso Putri <81726650+anggiethasantoso@users.noreply.github.com>
2023-05-06 14:21:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-import leafmap.foliumap as leafmap
 import plotly.express as px
 import plotly.graph_objects as go
 from streamlit_option_menu import option_menu
